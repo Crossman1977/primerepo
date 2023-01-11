fd6f239fcffa76fcc0a8704783eff8674ffe2586
crossman1977 <sabrinaper29@gmail.com>
2023-01-11 02:32:49 +0100

    cambio

--- function.js
+++ function.js
@@ -4,12 +4,7 @@ document.getElementById("boton").onclick = function (){
     document.getElementById("demo").innerHTML = 
     "<li>  Operador de Pc- Data Entry </li> <li>  Analista de Programas - Disco </li> <li>  Tecnico en Refrigeracion - Refrigeracion Quequen </li>";
 
-    document.getElementById("boton1").onclick = function (){
-        console.log();
-        document.getElementById("tutu").innerHTML = 
-        " <li>  email:eltutu2036@gmail.com </li>";
-    
+ 
 
 
     }
-}
